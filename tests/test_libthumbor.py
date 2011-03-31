ca82cc4456389e3ba1b114f9a7608663ff4ecdcd
Bernardo Heynemann <heynemann@gmail.com>
2011-03-31 16:38:06 -0300

    Verifying libthumbor url matches thumbor url for same image and same key

--- tests/test_libthumbor.py
+++ tests/test_libthumbor.py
@@ -8,16 +8,34 @@
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
+from thumbor.crypto import Crypto
 from libthumbor import CryptoURL
 
 def test_usage():
-    crypto = CryptoURL(key='my-security-key')
+    key = "my-security-key"
+    image = "s.glbimg.com/et/bb/f/original/2011/03/24/VN0JiwzmOw0b0lg.jpg"
+    thumbor_crypto = Crypto(salt=key)
+
+    thumbor_options = thumbor_crypto.encrypt(
+        300,
+        200,
+        True,
+        False,
+        False,
+        'center',
+        'middle',
+        0, 0, 0, 0,
+        image=image
+    )
+    thumbor_url = "/%s/%s" % (thumbor_options, image)
+
+    crypto = CryptoURL(key=key)
 
     url = crypto.generate(
         width=300,
         height=200,
         smart=True,
-        image_url='s.glbimg.com/et/bb/f/original/2011/03/24/VN0JiwzmOw0b0lg.jpg'
+        image_url=image
     )
 
-    assert url == "/mjFhjvFRAj7oAfr1UZuGLXXjEI9yKAoTkrDhF5xnXLeF6CSX6_ZPbpqlBE_AhwZT/s.glbimg.com/et/bb/f/original/2011/03/24/VN0JiwzmOw0b0lg.jpg"
+    assert url == thumbor_url
