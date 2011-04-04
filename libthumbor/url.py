8a657bdb2a0dec9b5109f7053d496456c16ecd5f
Bernardo Heynemann <heynemann@gmail.com>
2011-04-03 22:42:08 -0300

    More tests included

--- libthumbor/url.py
+++ libthumbor/url.py
@@ -4,7 +4,7 @@
 # libthumbor - python extension to thumbor
 # https://github.com/heynemann/libthumbor/wiki
 
-# Licensed under the MIT license: 
+# Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
@@ -12,12 +12,62 @@
 
 import hashlib
 
+AVAILABLE_HALIGN = ['left', 'center', 'right']
+AVAILABLE_VALIGN = ['top', 'middle', 'bottom']
+
+def calculate_width_and_height(url_parts, options):
+    '''Appends width and height information to url'''
+    width = options.get('width', 0)
+    has_width = width
+    height = options.get('height', 0)
+    has_height = height
+
+    flip = options.get('flip', False)
+    flop = options.get('flop', False)
+
+    if flip:
+        width = width * -1
+    if flop:
+        height = height * -1
+
+    if not has_width and not has_height:
+        if flip:
+            width = "-0"
+        if flop:
+            height = "-0"
+
+    if width or height:
+        url_parts.append('%sx%s' % (width, height))
+
 def url_for(**options):
     '''Returns the url for the specified options'''
 
     if 'image_url' not in options:
         raise RuntimeError('The image_url argument is mandatory.')
-    
-    image_hash = hashlib.md5(options['image_url']).hexdigest()
 
-    return image_hash
+    url_parts = []
+
+    calculate_width_and_height(url_parts, options)
+
+    halign = options.get('halign', 'center')
+    valign = options.get('valign', 'middle')
+
+    if not halign in AVAILABLE_HALIGN:
+        raise ValueError('Only "left", "center" and "right" are' + \
+                         ' valid values for horizontal alignment.')
+    if not valign in AVAILABLE_VALIGN:
+        raise ValueError('Only "top", "middle" and "bottom" are' + \
+                         ' valid values for vertical alignment.')
+
+    if halign != 'center':
+        url_parts.append(halign)
+    if valign != 'middle':
+        url_parts.append(valign)
+
+    if options.get('smart', False):
+        url_parts.append('smart')
+
+    image_hash = hashlib.md5(options['image_url']).hexdigest()
+    url_parts.append(image_hash)
+
+    return "/".join(url_parts)
--- tests/test_url_composer.py
+++ tests/test_url_composer.py
@@ -4,7 +4,7 @@
 # libthumbor - python extension to thumbor
 # https://github.com/heynemann/libthumbor/wiki
 
-# Licensed under the MIT license: 
+# Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 Bernardo Heynemann heynemann@gmail.com
 
@@ -19,7 +19,7 @@ IMAGE_MD5 = '84996242f65a4d864aceb125e1c4c5ba'
 
 def decrypt_in_thumbor(key, encrypted):
     '''Uses thumbor to decrypt libthumbor's encrypted URL'''
-    crypto = Crypto(key)    
+    crypto = Crypto(key)
     return crypto.decrypt(encrypted)
 
 def test_no_options_specified():
@@ -50,4 +50,267 @@ def test_url_raises_if_no_url():
         assert str(err) == 'The image_url argument is mandatory.'
         return True
     assert False, 'Should not have gotten this far'
+
+def test_url_width_height_1():
+    '''test_url_width_height_1
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a width of 300
+    When
+        I ask my library for an URL
+    Then
+        I get "300x0/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(width=300, image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "300x0/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_url_width_height_2():
+    '''test_url_width_height_2
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a height of 300
+    When
+        I ask my library for an URL
+    Then
+        I get "0x300/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(height=300, image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "0x300/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_url_width_height_3():
+    '''test_url_width_height_3
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a width of 200
+        And a height of 300
+    When
+        I ask my library for an URL
+    Then
+        I get "200x300/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(width=200,
+                  height=300,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "200x300/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_smart_url():
+    '''test_smart_url
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a width of 200
+        And a height of 300
+        And the smart flag
+    When
+        I ask my library for an URL
+    Then
+        I get "200x300/smart/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(width=200,
+                  height=300,
+                  smart=True,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "200x300/smart/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_flip_1():
+    '''test_flip_1
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And the flip flag
+    When
+        I ask my library for an URL
+    Then
+        I get "-0x0/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(flip=True,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "-0x0/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_flip_2():
+    '''test_flip_2
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a width of 200
+        And the flip flag
+    When
+        I ask my library for an URL
+    Then
+        I get "-200x0/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(flip=True,
+                  width=200,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "-200x0/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_flop_1():
+    '''test_flop_1
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And the flop flag
+    When
+        I ask my library for an URL
+    Then
+        I get "0x-0/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(flop=True,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "0x-0/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_flop_2():
+    '''test_flop_2
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a height of 200
+        And the flop flag
+    When
+        I ask my library for an URL
+    Then
+        I get "0x-200/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(flop=True,
+                  height=200,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "0x-200/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_flip_flop():
+    '''test_flip_flop
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And the flip flag
+        And the flop flag
+    When
+        I ask my library for an URL
+    Then
+        I get "-0x-0/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(flip=True,
+                  flop=True,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "-0x-0/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_flip_flop2():
+    '''test_flip_flop2
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a width of 200
+        And a height of 300
+        And the flip flag
+        And the flop flag
+    When
+        I ask my library for an URL
+    Then
+        I get "-200x-300/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(flip=True,
+                  flop=True,
+                  width=200,
+                  height=300,
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == "-200x-300/84996242f65a4d864aceb125e1c4c5ba", url
+
+def test_horizontal_alignment():
+    '''test_horizontal_alignment
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a 'left' horizontal alignment option
+    When
+        I ask my library for an URL
+    Then
+        I get "left/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(halign='left',
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == 'left/84996242f65a4d864aceb125e1c4c5ba', url
+
+def test_horizontal_alignment2():
+    '''test_horizontal_alignment2
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a 'center' horizontal alignment option
+    When
+        I ask my library for an URL
+    Then
+        I get "84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(halign='center',
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == '84996242f65a4d864aceb125e1c4c5ba', url
+
+def test_vertical_alignment():
+    '''test_vertical_alignment
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a 'top' vertical alignment option
+    When
+        I ask my library for an URL
+    Then
+        I get "top/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(valign='top',
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == 'top/84996242f65a4d864aceb125e1c4c5ba', url
+
+def test_vertical_alignment2():
+    '''test_vertical_alignment2
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a 'middle' vertical alignment option
+    When
+        I ask my library for an URL
+    Then
+        I get "84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(valign='middle',
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == '84996242f65a4d864aceb125e1c4c5ba', url
+
+def test_both_alignments():
+    '''test_both_alignments
+    Given
+        An image URL of "my.server.com/some/path/to/image.jpg"
+        And a 'left' horizontal alignment option
+        And a 'top' vertical alignment option
+    When
+        I ask my library for an URL
+    Then
+        I get "left/top/84996242f65a4d864aceb125e1c4c5ba" as URL
+    '''
+    url = url_for(halign='left',
+                  valign='top',
+                  image_url="my.server.com/some/path/to/image.jpg")
+
+    assert url == 'left/top/84996242f65a4d864aceb125e1c4c5ba', url
+
+def test_proper_haligns():
+    '''test_proper_haligns'''
+    try:
+        url_for(halign='wrong', image_url=IMAGE_URL)
+    except ValueError, err:
+        assert str(err) == 'Only "left", "center" and "right"' + \
+                           ' are valid values for horizontal alignment.'
+        return True
+    assert False, "Should not have gotten this far."
+
+def test_proper_valigns():
+    '''test_proper_haligns'''
+    try:
+        url_for(valign='wrong', image_url=IMAGE_URL)
+    except ValueError, err:
+        assert str(err) == 'Only "top", "middle" and "bottom"' + \
+                           ' are valid values for vertical alignment.'
+        return True
+    assert False, "Should not have gotten this far."
 
